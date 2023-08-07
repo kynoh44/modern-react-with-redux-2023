2ef488cb0ea23622602de716b4ec78c79280877a
kynoh <nohkr160428@gmail.com>
2023-08-07 13:43:43 +0900

    Section 2-10. Showing Basic Content # What is the difference between React and ReactDOM libraries? # Explain all the steps # What does process JSX in the function component?

--- jsx/src/index.js
+++ jsx/src/index.js
@@ -1,17 +1,17 @@
-// 1) Import the React and ReactDOM libraries
+/* 1) Import the React and ReactDOM libraries */
 import React from	'react';
 import ReactDOM from 'react-dom/client';
 
-// 2) Get a reference to the div with ID root
+/* 2) Get a reference to the div with ID root */
 const el = document.getElementById('root');
 
-// 3) Tell React to take control of that element
+/* 3) Tell React to take control of that element */
 const root = ReactDOM.createRoot(el);
 
-// 4) Create a component
+/* 4) Create a component */
 function App() {
 	return <h1>Hi there!</h1>;
 }
 
-// 5) Show the component on the screen
+/* 5) Show the component on the screen */
 root.render(<App />);
