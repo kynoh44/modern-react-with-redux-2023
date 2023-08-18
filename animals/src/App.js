6238b4817ef03587a72a7c9df7a9e5e4d59521cf
kynoh <nohkr160428@gmail.com>
2023-08-18 15:57:34 +0900

    use useState with random animal

--- animals/src/App.js
+++ animals/src/App.js
@@ -1,18 +1,23 @@
 import { useState } from 'react';
 
+function getRandomAnimal() {
+	const animals = ['bird', 'cat', 'cow', 'dog', 'gator', 'horse'];
+
+	return animals[Math.floor(Math.random() * animals.length)]
+}
+
+
 function App() {
-	const [count, setCount] = useState(0);
+	const [animals, setAnimals] = useState([]);	
 
 	const handleClick = () => {
-		setCount(count + 1);
+		setAnimals([...animals, getRandomAnimal()]);
 	};
-
-	console.log(useState(50));
-
+	
 	return (
 		<div>
 			<button onClick={handleClick}>Add Animal</button>
-			<div>Number of animals: {count}</div>
+			<div>{animals}</div>
 		</div>
 	);
 }
