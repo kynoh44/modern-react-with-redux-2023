a5087e769365145172d6dba1d899368b91943f2d
kynoh <nohkr160428@gmail.com>
2023-08-18 15:01:56 +0900

    Array destructuring and useState()

--- animals/src/App.js
+++ animals/src/App.js
@@ -1,39 +1,20 @@
 import { useState } from 'react';
 
 function App() {
+	const [count, setCount] = useState(0);
 
-	/* Test of array destructuring */
-	function makeArray() {
-		return [1, 10, 32, 40];
-	}
+	const handleClick = () => {
+		setCount(count + 1);
+	};
 
-	/* Test 0 */
-	//const myArray = makeArray();
-	//const firstElement = myArray[0];
-	//const secondElement = myArray[1];
-	//console.log(firstElement, secondElement);
+	console.log(useState(50));
 
-	/* Test 1 */
-	const [firstElement, secondElement] = makeArray();
-	console.log(firstElement, secondElement);
-
-
-
-
-
-
-	//const [count, setCount] = useState(0);
-
-	//const handleClick = () => {
-	//	setCount(count + 1);
-	//};
-
-	//return (
-	//	<div>
-	//		<button onClick={handleClick}>Add Animal</button>
-	//		<div>Number of animals: {count}</div>
-	//	</div>
-	//);
+	return (
+		<div>
+			<button onClick={handleClick}>Add Animal</button>
+			<div>Number of animals: {count}</div>
+		</div>
+	);
 }
 
 export default App;
