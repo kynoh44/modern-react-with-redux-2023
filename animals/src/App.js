b6cce4f429fdc1b57f5f1e77a91f90f52f434cbb
kynoh <nohkr160428@gmail.com>
2023-08-18 13:04:11 +0900

    Define arrow function and reference in the app function.

--- animals/src/App.js
+++ animals/src/App.js
@@ -3,12 +3,19 @@ function App() {
 	Inline arrow function is common in professional projects.
 	*/
 	
+	// Define the arrow function
+	const handleClick = () => {
+		console.log('Hello, there!');
+	};
+
 	return (
 		<div>
 			<button
 				onClick={() => console.log('Button was clicked!')}>
 					Add Animal
 			</button>
+
+			<button onClick={handleClick}>Test button</button>
 		</div>
 	);
 }
