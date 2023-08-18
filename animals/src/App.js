79a51d5740756f3165a135db25b04f958d63d0f5
kynoh <nohkr160428@gmail.com>
2023-08-18 11:23:41 +0900

    4-43. Detail of event statement Community convention for event handler and understanding of event handler

--- animals/src/App.js
+++ animals/src/App.js
@@ -1,11 +1,19 @@
 function App() {
-	const handleClick = () => {
+	const handleMouseMove = () => {
 		console.log('Button was clicked!')
 	};
+	/*
+		This function is an Event handler or a Callback function
+		the name of event handler is recommended to be named like "handle + EventName".
+	*/
 
-	return	<div>
-		<button onClick={handleClick}>Add Animal</button>
-	</div>;
+	return (
+		<div>
+		<button onMouseMove={handleMouseMove}>
+			Add Animal
+		</button>
+	</div>
+	);
 }
 
 export default App;
