8bd6a24c4584e45d38260e169d9721d1710476e2
kynoh <nohkr160428@gmail.com>
2023-08-27 13:42:13 +0900

    Test of corner cases

--- jsx/src/index.js
+++ jsx/src/index.js
@@ -11,7 +11,28 @@ const root = ReactDOM.createRoot(el);
 
 /* 4) Create a component */
 function App() {
-	return <h1>{new Date().toLocaleDateString()}</h1>;
+	let ccBool = true;
+	let ccKeyword = undefined;
+	let ccNull = null;
+	let ccArray = [1, 2, 3, "hello", undefined];
+
+	return (
+		<div>
+			<h1>{ccBool}</h1>
+			<h1>bool type cannot be displayed!</h1>
+
+			<h2>{ccKeyword}</h2>
+			<h2>Keyword cannot be displayed</h2>
+			<div>
+				<ul>
+					<li>{ccNull}</li>
+					<li>Above null cannot be displayed</li>
+					<li>{ccArray}</li>
+					<li>All elements of the array are displayed</li>
+				</ul>
+			</div>
+		</div>
+	);
 }
 
 /* 5) Show the component on the screen */
