9d6b564c183d727e90e241bba67d96ac0f1c576e
kynoh <nohkr160428@gmail.com>
2023-08-27 11:32:50 +0900

    JSX must be returned in the function to be shown

--- jsx/src/index.js
+++ jsx/src/index.js
@@ -1,7 +1,7 @@
 /* 1) Import the React and ReactDOM libraries */
 import React from	'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
+//import App from './App';
 
 /* 2) Get a reference to the div with ID root */
 const el = document.getElementById('root');
@@ -9,5 +9,11 @@ const el = document.getElementById('root');
 /* 3) Tell React to take control of that element */
 const root = ReactDOM.createRoot(el);
 
-/* 4) Show the component on the screen */
+/* 4) Create a component */
+function App() {
+	<h3>Bye there!</h3>	// Not returned, so not shown
+	return <h1>Hi there!</h1>;
+}
+
+/* 5) Show the component on the screen */
 root.render(<App />);
