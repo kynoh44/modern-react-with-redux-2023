124472b0e850742ea0dc92cd92b1137c84ffa207
kynoh <nohkr160428@gmail.com>
2023-08-28 13:00:12 +0900

    Test 1 of Using Props for Communication with Parent-Child Components
    Reference : 🔑Parent-Child Component에서 Props 사용 4 단계,
    💡Props에 JS Object를 Reference

--- pdas/src/App.js
+++ pdas/src/App.js
@@ -1,11 +1,20 @@
 import ProfileCard from './ProfileCard';
 
+/* Create props variable and test 1 */
+
+const custStyle = {
+	backgroundColor: "black",
+	color: "gold"
+}
+
 function App() {
 	return (
 		<div>
 			<div>Personal Digital Assistants</div>
 
-			<ProfileCard color= "red" />
+			<ProfileCard
+				backgroundColor= { custStyle.backgroundColor }
+				color= {custStyle.color} />
 			<ProfileCard color= "blue"/>
 			<ProfileCard color= "gold"/>
 		</div>
