4587ed14b72d6ef95602b3bf26dd58c55a73f068
kynoh <nohkr160428@gmail.com>
2023-08-28 17:24:33 +0900

    Reference image variables into the App component and Modify the child component Reference : 🔑Props로 Image 접근

--- pdas/src/ProfileCard.js
+++ pdas/src/ProfileCard.js
@@ -1,8 +1,9 @@
-function ProfileCard({ title, handle }) {
+function ProfileCard({ title, handle, image }) {
 	return (
 		<div>
-			<div>Title is { title }</div>
-			<div>Handle is { handle }</div>
+			<img src={image} />
+			<div>Title is {title}</div>
+			<div>Handle is {handle}</div>
 		</div>
 	);
 }
