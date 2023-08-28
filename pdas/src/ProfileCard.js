ef85a12c6ce94f061a511e985bd4addb57e03a7b
kynoh <nohkr160428@gmail.com>
2023-08-28 22:52:36 +0900

    Use Props with CSS styles from Bulma in Child Component Reference : 🔑Component에 CSS 적용

--- pdas/src/ProfileCard.js
+++ pdas/src/ProfileCard.js
@@ -1,9 +1,17 @@
 function ProfileCard({ title, handle, image }) {
 	return (
-		<div>
-			<img src={image} alt="PDA Loge"/>
-			<div>Title is {title}</div>
-			<div>Handle is {handle}</div>
+		<div className="card">
+			<div className="card-image">
+				<figure className="image is-1by1">
+					<img src={image} alt="PDA Loge"/>
+				</figure>
+			</div>
+			<div className="card-content">
+				<div className="media-content">
+					<p className="title is-4">{title}</p>
+					<p className="subtitle is-6">{handle}</p>
+				</div>
+			</div>
 		</div>
 	);
 }
