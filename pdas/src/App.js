45ba27624c6939543677014f7f8aea9df39b8f6f
kynoh <nohkr160428@gmail.com>
2023-08-28 23:15:18 +0900

    Use CSS in App Component with columns and Modify How import CSS Library Reference : 🔑Component에 CSS 적용 🔑CSS Library를 Import

--- pdas/src/App.js
+++ pdas/src/App.js
@@ -2,16 +2,27 @@ import ProfileCard from './ProfileCard';
 import AlexaImage from './images/alexa.png';
 import CortanaImage from './images/cortana.png';
 import SiriImage from './images/siri.png';
-import Bulma from 'bulma/css/bulma.css';
+import 'bulma/css/bulma.css';
 
 function App() {
 	return (		
 		<div>
 			<div>Personal Digital Assistants</div>
-			
-			<ProfileCard title = "Alexa" handle = "@alexa99" image = {AlexaImage} />
-			<ProfileCard title = "Cortana" handle = "@cortana32" image = {CortanaImage} />
-			<ProfileCard title = "Siri" handle = "@siri01" image = {SiriImage} />
+			<div className="container">
+				<section className="section">
+					<div className="columns">
+						<div className="column is-4">
+						<ProfileCard title="Alexa" handle="@alexa99" image={AlexaImage} />
+						</div>
+						<div className="column is-4">
+							<ProfileCard title="Cortana" handle="@cortana32" image={CortanaImage} />						
+						</div>
+						<div className="column is-4">
+							<ProfileCard title="Siri" handle="@siri01" image={SiriImage} />							
+						</div>
+					</div>
+				</section>
+			</div>			
 		</div>
 	);
 }
