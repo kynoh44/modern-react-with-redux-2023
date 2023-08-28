59fca837d4d40e0b3d3802d77a8595bf3ff9a8ad
kynoh <nohkr160428@gmail.com>
2023-08-28 10:28:35 +0900

    Use of Module System

--- jsx/src/App.js
+++ jsx/src/App.js
@@ -2,10 +2,18 @@
 //App.js file
 
 /* 2) Make a component with returning JSX */
+
+let message = "MyApp";
+
 function App() {
-	return <h1>Bye there!</h1>;
+	return(
+	<div>
+		<h1>Bye there!</h1>
+		<p>{ message }</p>
+	</div>
+	);
 }
 
 /* 3) Export the component at the bottom */
 export default App;
-export const message = "MyApp";
+export { message };
--- jsx/src/index.js
+++ jsx/src/index.js
@@ -2,14 +2,14 @@ import React from	'react';
 import ReactDOM from 'react-dom/client';
 
 /* 4) Import the component file */
-import MyApp from './App';	// Renamed variable
-//import MyApp, { message } from './App';
+//import MyApp from './App';	// Renamed variable
+import MyApp, { message } from './App';	// Import named exports
 
 const el = document.getElementById('root');
 const root = ReactDOM.createRoot(el);
 
 //function Named() {
-//	return message;
+//	return <h1>{ message }</h1>;
 //}
 
 /* 5) Use the component */
