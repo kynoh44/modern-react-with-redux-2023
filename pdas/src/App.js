50ea4dc461622ec944d30a0a05a7493db7592c2c
kynoh <nohkr160428@gmail.com>
2023-08-28 23:46:22 +0900

    Use CSS in App Component with columns and Modify How import CSS Library Reference : 🔑Component에 CSS 적용 🔑CSS Library를 Import

--- pdas/src/App.js
+++ pdas/src/App.js
@@ -12,7 +12,7 @@ function App() {
 				<section className="section">
 					<div className="columns">
 						<div className="column is-4">
-						<ProfileCard title="Alexa" handle="@alexa99" image={AlexaImage} />
+							<ProfileCard title="Alexa" handle="@alexa99" image={AlexaImage} />
 						</div>
 						<div className="column is-4">
 							<ProfileCard title="Cortana" handle="@cortana32" image={CortanaImage} />						
