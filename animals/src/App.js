7421bf116e3ad80cf06b17fbb7b2c07bdb4fcd31
kynoh <nohkr160428@gmail.com>
2023-08-29 23:37:48 +0900

    Use of useState() with function defining of event handler and reference to props in JSX element, use of updated data in Parent component
    Reference : 🔑useState() 사용

--- animals/src/App.js
+++ animals/src/App.js
@@ -2,10 +2,14 @@ import { useState } from "react";
 
 function App() {
 	const [count, setCount] = useState(0);
+	
+	function handleClick() {
+		setCount(count + 1);
+	} 
 
 	return (
 		<div>
-			<button onClick={() => setCount(count + 1)}>Add Animal</button>
+			<button onClick={handleClick}>Add Animal</button>
 			<div>Number of animals: {count}</div>
 		</div>
 	);
