61b97ebb4058c625749c42fc63698a60cbcb8626
kynoh <nohkr160428@gmail.com>
2023-08-30 12:53:58 +0900

    event handler and state system combination with arrow function

--- animals/src/App.js
+++ animals/src/App.js
@@ -1,15 +1,12 @@
 import { useState } from "react";
 
 function App() {
-	const [count, setCount] = useState(110);
-	
-	function handleClick() {
-		setCount(count + 1);
-	} 
+	const [count, setCount] = useState(0);
 
-	function handleClick() {
+	const handleClick = () => {
 		setCount(count + 1);
 	}
+
 	return (
 		<div>
 			<button onClick={handleClick}>Add Animal</button>
