cca57db2b13d18a38712a9f22dbda71cdb18901b
kynoh <nohkr160428@gmail.com>
2023-09-04 09:55:38 +0900

    Modify some mistakes for Request and Insepct by Developer tools

--- pics/src/api.js
+++ pics/src/api.js
@@ -1,13 +1,18 @@
 import axios from 'axios';
 
-const searchImages = () => {
-	axios.get('https://api.unsplash.com/search/photos', {
+const searchImages = async () => {
+	const response = await axios.get('https://api.unsplash.com/search/photos', {
 		headers: {
-			Authorization: 'Client-ID wa1M8FE9yVnYaAPnBjJ9znjIFOVz78cjIP0r73RNV7s'
+			Authorization: 'Client-ID wa1M8FE9yVnYaAPnBjJ9znjIFOVz78cjIP0r73RNV7s',
 		},
 		params: {
-			query: 'cars'	// User Input
+			query: 'cars',	// User Input
 		}
-	}
-	);
+	});
+
+	console.log(response);
+
+	return response;
 };
+
+export default searchImages;
