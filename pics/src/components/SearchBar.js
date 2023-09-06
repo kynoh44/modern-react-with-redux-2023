4aa12b0c5c627adb5b9c2d97d12104dd905e8613
kynoh <nohkr160428@gmail.com>
2023-09-06 17:07:38 +0900

    Handling input element with using state system, without access to the value of input element directly

--- pics/src/components/SearchBar.js
+++ pics/src/components/SearchBar.js
@@ -1,15 +1,25 @@
-function SearchBar() {
+import { useState } from "react";
+
+function SearchBar({ onSubmit }) {
+	const [term, setTerm] = useState("");
+
 	const handleFormSubmit = (event) => {
 		event.preventDefault();
-		
-		/* NEVER EVER EVER DO THIS! */
-		//onSubmit(document.querySelector('input').value);
+		onSubmit(term);
+	};
+
+	const handleChange = (event) => {
+		//console.log(event);
+		//console.log(event.target.value);
+		setTerm(event.target.value);
 	};
 
 	return(
 		<div>
 			<form onSubmit={handleFormSubmit}>
-				<input />
+				{/*<section>You searched : {term}</section>*/}
+				<input value={term} onChange={handleChange}/>
+				{/*<section>{term.length < 3 && 'Wait! Term must be longer'}</section>*/}
 			</form>
 		</div>
 	);
