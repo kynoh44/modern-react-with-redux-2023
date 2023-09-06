62993c59428538d47fed494d97a6ca493c25e65b
kynoh <nohkr160428@gmail.com>
2023-09-06 13:44:52 +0900

    Handling form element with event.preventDefault

--- pics/src/App.js
+++ pics/src/App.js
@@ -3,7 +3,6 @@ import SearchBar from "./components/SearchBar";
 function App() {
 	const handleSubmit = (term) => {
 		console.log("Do a search with", term);
-		//searchImages(term);
 	};
 
 	return(
--- pics/src/components/SearchBar.js
+++ pics/src/components/SearchBar.js
@@ -1,13 +1,17 @@
-function SearchBar({ onSubmit }) {
-	const handleClick = () => {
-		onSubmit("cars");
+function SearchBar() {
 
-	}
+	const handleFormSubmit = (event) => {
+		event.preventDefault();
+			console.log("I need to tell the parent about some data");
+	};
+
 	return(
 		<div>
-			<input />
-			<button onClick={handleClick}>Click me</button>
-		</div>);
+			<form onSubmit={handleFormSubmit}>
+				<input />
+			</form>
+		</div>
+	);
 }
 
 export default SearchBar;
