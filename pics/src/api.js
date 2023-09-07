1d0df7fddbf3fbc2adb305741c49e0be282c4037
kynoh <nohkr160428@gmail.com>
2023-09-07 12:28:01 +0900

    Using API with user input

--- pics/src/api.js
+++ pics/src/api.js
@@ -1,16 +1,16 @@
 import axios from 'axios';
 
-//const searchImages = async (term) => {
-//	const response = await axios.get('https://api.unsplash.com/search/photos', {
-//		headers: {
-//			Authorization: 'Client-ID wa1M8FE9yVnYaAPnBjJ9znjIFOVz78cjIP0r73RNV7s',
-//		},
-//		params: {
-//			query: term	// User Input
-//		}
-//	});
+const searchImages = async (term) => {
+	const response = await axios.get('https://api.unsplash.com/search/photos', {
+		headers: {
+			Authorization: 'Client-ID wa1M8FE9yVnYaAPnBjJ9znjIFOVz78cjIP0r73RNV7s',
+		},
+		params: {
+			query: term	// User Input
+		}
+	});
 
-//	return response.data.results;
-//};
+	return response.data.results;
+};
 
-//export default searchImages;
+export default searchImages;
