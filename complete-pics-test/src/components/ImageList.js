48e7c5b28d326c00e0cf400f310799d57a07ab2e
kynoh <nohkr160428@gmail.com>
2023-09-10 01:26:15 +0900

    Complete pics test

--- complete-pics-test/src/components/ImageList.js
+++ complete-pics-test/src/components/ImageList.js
@@ -3,8 +3,11 @@ import "../css/ImageList.css";
 
 function ImageList({ images }) {
 	const imgObjToCompArr = images.map((image, key) => {
-		return <ImageShow key={image.id} image={image} />
+		return(
+			<ImageShow key={image.id} image={image} />
+		);
 	});
+
 	return(
 		<div className="image-list">
 			{imgObjToCompArr}
