39935f357290e45f3c488b88683275d8023afe62
kynoh <nohkr160428@gmail.com>
2023-09-14 14:17:49 +0900

    Feature of delete a book by clicking button
    with some styles
    Reference notes

--- books/src/components/BookList.js
+++ books/src/components/BookList.js
@@ -1,8 +1,8 @@
 import BookShow from "./BookShow";
 
-function BookList({ books }) {
+function BookList({ books, onDelete }) {
 	const renderedBook = books.map((book) => {
-		return(<BookShow key={book.id} book={book} />);
+		return(<BookShow key={book.id} book={book} onDelete={onDelete}/>);
 	});
 
 	return(
