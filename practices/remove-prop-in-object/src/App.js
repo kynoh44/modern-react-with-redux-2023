3700f1a816edb4290c905f4bc533c13d64a821c4
kynoh <nohkr160428@gmail.com>
2023-09-14 12:18:04 +0900

    Remove prop from object in state by object destructuring

---
+++ practices/remove-prop-in-object/src/App.js
@@ -0,0 +1,24 @@
+
+import { useState } from 'react';
+
+function App() {
+	const [obj, setObj] = useState(
+		{ id: 0, val: 1, type: "int", alias: "i" }
+	);
+	
+	const removeType = () => {
+		/* 삭제할 prop을 제외한 object 생성 by Object Destructuring */
+		const { type, ...rest } = obj;	// type과 나머지를 합친게 위에 초기화한 obj라는 의미
+		setObj(rest);
+		console.log(rest);
+	};
+
+	return(
+		<div>
+			<p>Remove Type!</p>
+			<button onClick={removeType}>Remove!</button>
+		</div>
+	);
+}
+
+export default App;
