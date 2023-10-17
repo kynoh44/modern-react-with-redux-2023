1ddbc104212e70bc96016335572373905e74088d
kynoh <nohkr160428@gmail.com>
2023-10-17 11:05:01 +0900

    Test for using context in BookList Component
    To modify the value of 'value' prop, need to do something another way

--- practices/books-with-context/src/component/BookList.js
+++ practices/books-with-context/src/component/BookList.js
@@ -1,12 +1,17 @@
 import BookShow from "./BookShow";
+import { useContext } from "react";
+import BookContext from "../context/books";
 
 function BookList({ books, onDelete, onEdit }) {
 	const renderedBooks = books.map((book) => {
 		return(<BookShow key={book.id} book={book} onDelete={onDelete} onEdit={onEdit} />)
 	});
 
+	const sharedData = useContext(BookContext);
+
 	return(
 		<div className="book-list">
+			{sharedData}
 			{renderedBooks}
 		</div>
 	);
