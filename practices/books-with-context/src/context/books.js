cb0ee824b6a3a09722559fd096be4e634d643bc0
kynoh <nohkr160428@gmail.com>
2023-10-17 12:29:28 +0900

    Define the single object in context file The single object(objShared) will be passed as value prop in the Provider component of context object(BookContext) The single object must be defined in the custom provider
    The custom provider returns children object which means child components that accesses and modifies the shared data of context (objShared.count)
    and children object must be wrapped with the built-in provider component of context object(BookContext)
    This return display Provider component of context object by custom provider component

--- practices/books-with-context/src/context/books.js
+++ practices/books-with-context/src/context/books.js
@@ -1,5 +1,24 @@
-import { createContext } from 'react';
+import { createContext, useState } from 'react';
 
 const BookContext = createContext();
 
+function Provider({ children }) {
+	const [count, setCount] = useState(5);
+
+	const objShared = {
+		count,
+		incrementCount: () => {
+			setCount(count + 1);
+			console.log("Shared data is clicked!");
+		}
+	};
+
+	return(
+		<BookContext.Provider value={objShared}>
+			{children}
+		</BookContext.Provider>
+	);
+}
+
 export default BookContext;
+export { Provider };
