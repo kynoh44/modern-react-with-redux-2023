6a6975b224dfb2222c33ed09c1c92938665e1f61
kynoh <nohkr160428@gmail.com>
2023-10-19 00:29:35 +0900

    Refactoring BookList, BooksShow components
    Refactoring BookList component
    
    Use the context object
    Import the context object(BookContext)
    Initialize an object to access to 'books' array member by useContext
    Remove the parameter of BookList component
    Initialized 'books' is used with map method
    
    NOTE: Prop system is not exclusive with context system
    Passing the book elements to BookShow by prop is easier then accessing every single book item to context
    That's the reason why 'key' and 'book' props are remained and passed
    
    Refactoring BookShow component
    
    Use the context object
    Import the context object(BookContext)
    Initialize an object for calling 'deleteBook' function by useContext
    And 'editBook' function member of the shared object is not needed in this component
    Because the function will be called in 'BookEdit' component
    
    Replace the use of 'onDelete' prop with 'deleteBook'
    Remove the paramters of 'handleSubmit' and the use of 'onEdit'
    Because 'editBook' function will be called in 'BookEdit' component
    And then 'id' and 'newTitle' will be submitted in that component
    
    This component(BookShow) must be able to display a book if it is edited
    Then 'onEditSubmit' prop must be passed to BookEdit component

--- books-with-context/src/components/BookList.js
+++ books-with-context/src/components/BookList.js
@@ -2,9 +2,11 @@ import BookShow from "./BookShow";
 import { useContext } from "react";
 import BooksContext from "../context/books";
 
-function BookList({ books, onDelete, onEdit }) {
+function BookList() {
+	const { books } = useContext(BooksContext);
+
 	const renderedBook = books.map((book) => {
-		return(<BookShow key={book.id} book={book} onDelete={onDelete} onEdit={onEdit}/>);
+		return(<BookShow key={book.id} book={book} />);
 	});
 
 	return(
--- books-with-context/src/components/BookShow.js
+++ books-with-context/src/components/BookShow.js
@@ -1,20 +1,22 @@
-import { useState } from "react";
+import { useContext, useState } from "react";
 import BookEdit from "./BookEdit";
+import BooksContext from 'react';
 
-function BookShow({ book, onDelete, onEdit }) {
+const { deleteBookById } = useContext(BooksContext);
+
+function BookShow({ book }) {
 	const [showEdit, setShowEdit] = useState(false);
 	
 	const handleDeleteClick = () => {
-		onDelete(book.id);
+		deleteBookById(book.id);
 	};
 
 	const handleEditClick = () => {
 		setShowEdit(!showEdit);
 	};
 
-	const handleSubmit = (id, newTitle) => {
+	const handleSubmit = () => {
 		setShowEdit(false);
-		onEdit(id, newTitle);
 	};
 
 	let content = <h3>{book.title}</h3>;
