7ae298bb9978ce2412e362d9f5cb861037333620
kynoh <nohkr160428@gmail.com>
2023-10-18 10:05:34 +0900

    Refactoring App component with context object
    Import the context object (BookContext) to call fetchBooks function in App component
    To call the function, initialize an object by destructuring an object
    And the member of declared object is fetchBooks and initialized with useContext the context object input as the argument

--- practices/books-with-context/src/App.js
+++ practices/books-with-context/src/App.js
@@ -1,12 +1,15 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useContext } from 'react';
 import BookCreate from './component/BookCreate';
 import BookList from './component/BookList';
 import axios from 'axios';
+import BookContext from './context/books';
 
 function App() {
 	useEffect(() => {
 		fetchBooks();
 	}, []);
+
+	const { fetchBooks } = useContext(BookContext);
 
 	return(
 		<div className='app'>
