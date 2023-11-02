95b5e623025976b4bb7892df7302384e6c24c309
kynoh <nohkr160428@gmail.com>
2023-11-02 23:36:48 +0900

    Remove element from state by index
    Backup

--- books/src/App.js
+++ books/src/App.js
@@ -5,8 +5,8 @@ function App() {
 	const [books, setBooks] = useState(["test0", "test1", "test2"]);
 	
 	const createBook = (title) => {
-		const updateBooks = books.filter( (book) => {
-			return title != book;
+		const updateBooks = books.filter( (book, idx) => {
+			return idx !== 1;
 		} );
 		setBooks(updateBooks);
 		console.clear();
