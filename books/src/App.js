69f23bba7b4ca6a06258b0edb9b76472a37df0aa
kynoh <nohkr160428@gmail.com>
2023-11-03 11:22:43 +0900

    Modify data in state as array of objects
    Use condition with member to specify the target object(s) to modify.
    
    Use spread operator <...> to copy objects that won't be modified.
    
    <map> method returns the elements of an array
    that meet the condition specified in a callback function.

--- books/src/App.js
+++ books/src/App.js
@@ -2,12 +2,21 @@ import { useState } from 'react';
 import BookCreate from './components/BookCreate';
 
 function App() {
-	const [books, setBooks] = useState(
-		{ id: 0, title: "test0", location: "A" }
-	);
+	const [books, setBooks] = useState([				//	State as an array of objects
+		{ id: 0, title: "test0", location: "A" },
+		{ id: 1, title: "test1", location: "B" },
+		{ id: 2, title: "test2", location: "C" },
+		{ id: 3, title: "test3", location: "D" }
+	]);
 	
 	const createBook = (title) => {
-		const updateBooks = { ...books, title: title, location: "B", pages: 259 };
+		const updateBooks = books.map((book) => {
+			if(book.id === 2) {
+				return { ...book, title: title, location: "Z" };	// member 추가는 안 돼요!
+			}
+			return book;
+		});
+
 		setBooks(updateBooks);
 		console.clear();
 	};
