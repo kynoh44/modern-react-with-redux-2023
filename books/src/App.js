0bdca1da306bec94519a6949216943a4acf102c2
kynoh <nohkr160428@gmail.com>
2023-11-08 13:34:43 +0900

    Refactoring with GET request (useEffect)
    Solve the infinite loop error with useEffect hook.
    <fetchBooks> is called only at the first rendering by the second argument of useEffect hook.
    
    If the second argument is an empty array, the body of arrow function in the first argument will be called only at the first rendering.

--- books/src/App.js
+++ books/src/App.js
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import BookCreate from './components/BookCreate';
 import BookList from './components/BookList';
 import axios from 'axios';
@@ -10,8 +10,10 @@ function App() {
 		const response = await axios.get("http://localhost:3001/books");
 		setBooks(response.data);
 	};
-	/* ERROR: Call of the function that requests with GET method occurs Infinite Loop!!! */
-	//fetchBook();
+
+	useEffect(() => {
+		fetchBooks();
+	}, []);
 
 	const createBook = async (title) => {
 		const response = await axios.post("http://localhost:3001/books", {
