4a8b6999b2c21b927db39af2d434d2a4cc9061e6
kynoh <nohkr160428@gmail.com>
2023-11-13 09:53:06 +0900

    Initialize <fetchBooks> in <App>
    Import <useContext>.
    Import <BookContext> (Context object in context file).
    Initialize <fetchBooks> with object destructuring using <useContext> and <BookContext>.
    
    <fetchBooks> will be the member of shared object.

--- books/src/App.js
+++ books/src/App.js
@@ -1,9 +1,11 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useContext } from 'react';
 import BookCreate from './components/BookCreate';
 import BookList from './components/BookList';
 import axios from 'axios';
+import BookContext from './context/books';
 
 function App() {
+	const { fetchBooks } = useContext(BookContext);
 
 	useEffect(() => {
 		fetchBooks();
