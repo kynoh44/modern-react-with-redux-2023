35e18e58bd6272990f1e01765b32b4a0fc55f804
kynoh <nohkr160428@gmail.com>
2023-11-13 09:04:48 +0900

    Move Functions using state to <Provider>

--- books/src/App.js
+++ books/src/App.js
@@ -9,43 +9,6 @@ function App() {
 		fetchBooks();
 	}, []);
 
-	const createBook = async (title) => {
-		const response = await axios.post("http://localhost:3001/books", {
-			title
-		});
-
-		const updateBooks = () => {
-			return [ ...books, response.data ];
-		};
-		setBooks(updateBooks);
-		console.clear();
-	};
-
-	const editBookById = async (id, title) => {	// Modify application state
-		const response = await axios.put(`http://localhost:3001/books/${id}`, {title});
-		const updateBooks = books.map((book) => {
-			if(id === book.id) {
-				return {...book, ...response.data};
-			}
-			console.log("PUT Response : ", response.data);
-			return book;
-		});
-
-		setBooks(updateBooks);
-	};
-
-	const deleteBookById = async (target) => {	// Modify application state
-		const response = await axios.delete(`http://localhost:3001/books/${target}`);
-		const updateBooks = books.filter((book) => {
-			console.log("target : ", target, "DELETE Response : ", response.request.responseURL);
-			return (target !== book.id);
-		});
-
-		setBooks(updateBooks);
-	};
-
-	console.log("State data : ", books);
-
 	return(
 		<div className='app'>
 			<h1>Reading List</h1>
--- books/src/context/books.js
+++ books/src/context/books.js
@@ -10,6 +10,40 @@ function Provider({ children }) {
 		setBooks(response.data);
 	};
 
+	const createBook = async (title) => {
+		const response = await axios.post("http://localhost:3001/books", {
+			title
+		});
+		const updateBooks = () => {
+			return [ ...books, response.data ];
+		};
+		setBooks(updateBooks);
+		console.clear();
+	};
+
+	const editBookById = async (id, title) => {	// Modify application state
+		const response = await axios.put(`http://localhost:3001/books/${id}`, {title});
+		const updateBooks = books.map((book) => {
+			if(id === book.id) {
+				return {...book, ...response.data};
+			}
+			console.log("PUT Response : ", response.data);
+			return book;
+		});
+		setBooks(updateBooks);
+	};
+
+	const deleteBookById = async (target) => {	// Modify application state
+		const response = await axios.delete(`http://localhost:3001/books/${target}`);
+		const updateBooks = books.filter((book) => {
+			console.log("target : ", target, "DELETE Response : ", response.request.responseURL);
+			return (target !== book.id);
+		});
+		setBooks(updateBooks);
+	};
+
+	console.log("State data : ", books);
+
 	return(
 		<BookContext.Provider value={{}}>
 			{children}
