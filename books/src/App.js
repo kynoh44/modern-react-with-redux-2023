567e85f7b81e8a6756d3a18c4b71cd05bed78eaa
kynoh <nohkr160428@gmail.com>
2023-11-23 18:57:34 +0900

    Save temporary

--- books/src/App.js
+++ books/src/App.js
@@ -1,12 +1,17 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import BookCreate from './components/BookCreate';
 import BookList from './components/BookList';
 import useBookContext from './hooks/use-book-context';
 
 function App() {
 	const { fetchBooks } = useBookContext();
+	const [ counter, setCounter ] = useState(0);
 
 	useEffect(() => {
+		document.body.onclick = () => {
+			console.log("body element is clicked! Counter is ", counter);
+		};
+
 		fetchBooks();
 	}, [fetchBooks]);
 
@@ -15,6 +20,8 @@ function App() {
 			<h1>Reading List</h1>
 			<BookList />
 			<BookCreate />
+			<button onClick={() => setCounter(counter + 1)}>CleanUp</button>
+			<div>Counter : {counter}</div>
 		</div>
 	);
 }
