9825b494f38c57cbc4415b7061007c2467a1b2d3
kynoh <nohkr160428@gmail.com>
2023-11-25 14:31:11 +0900

    How to validate prop

--- comps/src/Button.js
+++ comps/src/Button.js
@@ -8,6 +8,10 @@ function Button({
 	outline,
 	rounded
 }) {
+	if(primary && secondary) {
+		throw new Error("Only one of primary and secondary should be provided.");
+	}
+
 	return(
 		<button>{children}</button>
 	);
