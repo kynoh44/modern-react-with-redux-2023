0dfc27ecc79103bdc21da49c2fdad57361ff7f61
kynoh <nohkr160428@gmail.com>
2023-12-06 00:01:36 +0900

    Create <ButtonPage> Create this page component file in <pages>. Copy all code of <App> component, and then paste to <ButtonPage>.

---
+++ comps/src/pages/ButtonPage.js
@@ -0,0 +1,39 @@
+import Button from "./components/Button";
+import { GoBell, GoCloud, GoDatabase } from "react-icons/go";
+
+function App() {
+	const handleClick = () => {
+		console.log("Clicked!");
+	};
+
+	return(
+		<div>
+			<div>
+				<Button className="mb-5" onClick={handleClick} success rounded outline>
+					<GoBell />
+					Happy Hacking!
+				</Button>
+			</div>
+			<div>
+				<Button danger outline onMouseEnter={handleClick}>
+					<GoCloud />
+					Buy Now
+				</Button>
+			</div>
+			<div>
+				<Button onMouseLeave={handleClick} warning>
+					<GoDatabase />
+					Sell Now
+				</Button>
+			</div>
+			<div>
+				<Button primary outline >Report</Button>
+			</div>
+			<div>
+				<Button secondary rounded >Cancel</Button>
+			</div>
+		</div>
+	);
+}
+
+export default App;
