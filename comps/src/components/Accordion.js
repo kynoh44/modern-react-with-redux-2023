2c9019f3223d7989923311e95c675eceb2afd590
kynoh <nohkr160428@gmail.com>
2023-12-06 23:49:41 +0900

    Add condition for <expandedIndex>
    if user clicked section(index n) is expanded,
    then if branch will be executed.
    
    if then other sections will be collapsed.
    It means other indexes will be collapsed.

--- comps/src/components/Accordion.js
+++ comps/src/components/Accordion.js
@@ -3,7 +3,13 @@ import { useState } from "react";
 function Accordion({ items }) {
 	const [expandedIndex, setExpandedIndex] = useState(0);	// First accordion section will be index 0
 
-	const renderedItems = items.map((item) => {
+	const renderedItems = items.map((item, index) => {
+		if(index === expandedIndex) {
+			console.log("Expanded");
+		} else {
+			console.log("Collapsed");
+		}
+
 		return(
 			<div key={item.id}>
 				<div>{item.label}</div>
