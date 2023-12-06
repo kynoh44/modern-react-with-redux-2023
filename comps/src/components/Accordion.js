a5bef6168507180906df37a18fd6f52c4f824859
kynoh <nohkr160428@gmail.com>
2023-12-06 23:58:01 +0900

    Replace if branch with temporary variable
    <isExpanded> will be assigned with bool type data.
    Working process is totally same with if branch.

--- comps/src/components/Accordion.js
+++ comps/src/components/Accordion.js
@@ -4,11 +4,8 @@ function Accordion({ items }) {
 	const [expandedIndex, setExpandedIndex] = useState(0);	// First accordion section will be index 0
 
 	const renderedItems = items.map((item, index) => {
-		if(index === expandedIndex) {
-			console.log("Expanded");
-		} else {
-			console.log("Collapsed");
-		}
+		const isExpanded = index === expandedIndex;
+		console.log(isExpanded);	// true, two falses
 
 		return(
 			<div key={item.id}>
