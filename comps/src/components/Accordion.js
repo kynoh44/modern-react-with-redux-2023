ac2b0787f49b21fbd347e142b4dbe77e4d154564
kynoh <nohkr160428@gmail.com>
2023-12-06 23:43:40 +0900

    Import <useState> and Initialize it
    <expandedIndex> state variable is for accordion expanding and collapse.

--- comps/src/components/Accordion.js
+++ comps/src/components/Accordion.js
@@ -1,4 +1,8 @@
+import { useState } from "react";
+
 function Accordion({ items }) {
+	const [expandedIndex, setExpandedIndex] = useState(0);	// First accordion section will be index 0
+
 	const renderedItems = items.map((item) => {
 		return(
 			<div key={item.id}>
