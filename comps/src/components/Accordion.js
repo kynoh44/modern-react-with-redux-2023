ff4b93be897dad6b39f4e76928ea8b79b65bad43
kynoh <nohkr160428@gmail.com>
2023-12-07 03:32:35 +0900

    Longhand version of event handler
    This is fine, but not recommended in professional project.

--- comps/src/components/Accordion.js
+++ comps/src/components/Accordion.js
@@ -6,9 +6,13 @@ function Accordion({ items }) {
 	const renderedItems = items.map((item, index) => {
 		const isExpanded = index === expandedIndex;
 
+		const handleClick = () => {
+			setExpandedIndex(index);
+		};
+
 		return(
 			<div key={item.id}>
-				<div onClick={() => setExpandedIndex(index)}>{item.label}</div>
+				<div onClick={handleClick}>{item.label}</div>
 				<div>{isExpanded && <div>{item.content}</div>}</div>
 			</div>
 		);
