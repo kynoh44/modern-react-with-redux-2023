dfbe93c352e77bcd89a2bc97d8391f422aadcbbd
kynoh <nohkr160428@gmail.com>
2023-12-06 21:57:57 +0900

    Mapping <items> in <Accordion>
    Use <map> function and Don't forget about rules.

--- comps/src/components/Accordion.js
+++ comps/src/components/Accordion.js
@@ -1,5 +1,16 @@
 function Accordion({ items }) {
-	return(<div />);
+	const renderedItems = items.map((item) => {
+		return(
+			<div>
+				<div>{item.label}</div>
+				<div>{item.content}</div>
+			</div>
+		);
+	});
+
+	return(
+		<div>{renderedItems}</div>
+	);
 }
 
 export default Accordion;
