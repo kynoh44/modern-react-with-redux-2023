46a63cf4b414c93908bcaaa8108d516a5cf486ab
kynoh <nohkr160428@gmail.com>
2023-12-09 02:17:22 +0900

    Define <renderedOptions> with <map>
    Specify the value of <key> prop in <map> function with <options.value>.

--- comps/src/components/Dropdown.js
+++ comps/src/components/Dropdown.js
@@ -1,5 +1,18 @@
 function Dropdown({ options }) {
-	return <div>Dropdown</div>;
+	const renderedOptions = options.map((option) => {
+		return(
+			<div key={option.value}>
+				{option.label}
+			</div>
+		);
+	});
+
+	return(
+		<div>
+			<div>Select...</div>
+			<div>{renderedOptions}</div>
+		</div>
+	);
 }
 
 export default Dropdown;
