b88b1664a5af3bf6ab1bfc7a3ea6eb57edbea8e6
kynoh <nohkr160428@gmail.com>
2023-12-09 01:20:14 +0900

    Prepare <options> in <App> Define an array of objects which consists of <label> and <value> pairs.
    Reference to <options> to <options> prop specified for returnning of <Dropdown> in <App>.
    How <Dropdown> component makes dropdown list with <options> passed from parent?

--- comps/src/App.js
+++ comps/src/App.js
@@ -1,7 +1,13 @@
 import Dropdown from "./components/Dropdown";
 
 function App() {
-	return <Dropdown />;
+	const options = [
+		{ label: "Red", value: "red" },
+		{ label: "Green", value: "green" },
+		{ label: "Blue", value: "blue" },
+	];
+
+	return <Dropdown options={options} />;
 }
 
 export default App;
--- comps/src/components/Dropdown.js
+++ comps/src/components/Dropdown.js
@@ -1,4 +1,4 @@
-function Dropdown() {
+function Dropdown({ options }) {
 	return <div>Dropdown</div>;
 }
 
