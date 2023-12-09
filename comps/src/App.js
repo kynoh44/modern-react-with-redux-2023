dc4087e3f86a602324841f5847650244788822e4
kynoh <nohkr160428@gmail.com>
2023-12-10 02:57:46 +0900

    Implement controlled dropdown component
    Define <handleSelect>, specify <option> as its parameter, and call <setSelection> with input <option>.
    
    Then, user-clicked <option> will be passed to <App> and update <selection> via <setSelection>.
    
    Specify <onSelection> and <selection> props to call of <Dropdown> component in <App>.
    
    Specify references to <selection> and <onSelect> as the parameters of <Dropdown> component.
    
    Call <onSelect> with input <option> in <handleOptionClick> of <Dropdown> component.
    
    Initialize <content> let variable with <Select...> and make if branch to display user-clicked input <selection.label>.

--- comps/src/App.js
+++ comps/src/App.js
@@ -4,13 +4,18 @@ import Dropdown from "./components/Dropdown";
 function App() {
 	const [selection, setSelection] = useState(null);
 
+	const handleSelect = (option) => {
+		setSelection(option);
+		console.log("User-clicked : ", option);
+	};
+
 	const options = [
 		{ label: "Red", value: "red" },
 		{ label: "Green", value: "green" },
 		{ label: "Blue", value: "blue" },
 	];
 
-	return <Dropdown options={options} />;
+	return <Dropdown options={options} selection={selection} onSelect={handleSelect} />;
 }
 
 export default App;
