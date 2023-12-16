c29780dbad3c42f99fe53ea7661e87bd3267018b
kynoh <nohkr160428@gmail.com>
2023-12-17 01:23:01 +0900

    Return cleanUp function in <useEffect>
    Returned function will be called before calling the body of arrow function in <useEffect> at the re-rendering time.
    cleanUp function can stop calling the function in the body of arrow function.

--- comps/src/components/Dropdown.js
+++ comps/src/components/Dropdown.js
@@ -11,6 +11,10 @@ function Dropdown({ options, value, onChange }) {
 		};
 
 		document.addEventListener("click", handler, true);
+
+		return () => {	// cleanUp function
+			document.removeEventListener("click", handler);
+		};
 	}, []);
 
 	const handleClick = () => {
