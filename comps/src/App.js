27877d63d1c214ef7fb8a91dab391c00ca2d5f33
kynoh <nohkr160428@gmail.com>
2023-12-31 16:57:34 +0900

    Copy all code of <App> to <DropdownPage>

--- comps/src/App.js
+++ comps/src/App.js
@@ -1,25 +1,7 @@
-import { useState } from "react";
-import Dropdown from "./components/Dropdown";
-
 function App() {
-	const [selection, setSelection] = useState(null);
-
-	const handleSelect = (option) => {
-		setSelection(option);
-		//console.log("User-clicked : ", option);
-	};
-
-	const options = [
-		{ label: "Red", value: "red" },
-		{ label: "Green", value: "green" },
-		{ label: "Blue", value: "blue" },
-	];
-	console.log("-------React Rerender Started-----------");
 	return(
-		<div className="flex">
-			<a href="/dashboard">Dashboard</a>
-			<Dropdown options={options} value={selection} onChange={handleSelect} />
-			<Dropdown options={options} value={selection} onChange={handleSelect} />
+		<div>
+			App
 		</div>
 	);
 }
