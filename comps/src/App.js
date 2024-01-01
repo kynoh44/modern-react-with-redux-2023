ccbfad383dd3f56d11855b17f9f139393ba9a026
kynoh <nohkr160428@gmail.com>
2024-01-02 01:12:59 +0900

    Specify root route as <DropdownPage>

--- comps/src/App.js
+++ comps/src/App.js
@@ -7,12 +7,12 @@ function App() {
 	return(
 		<div>
 			<Link to="/accordion">Go to accordion</Link>
-			<Link to="/dropdown">Go to dropdown</Link>
+			<Link to="/">Go to dropdown</Link>
 			<div>
 				<Route path="/accordion">
 					<AccordionPage />
 				</Route>
-				<Route path="/dropdown">
+				<Route path="/">
 					<DropdownPage />
 				</Route>
 			</div>
