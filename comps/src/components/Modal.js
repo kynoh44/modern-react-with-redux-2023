447153c39cd404e4acd9f3d5b33373c5e1898ba7
kynoh <nohkr160428@gmail.com>
2024-01-27 15:50:47 +0900

    Prevent scroll action on <Modal> is displayed.

--- comps/src/components/Modal.js
+++ comps/src/components/Modal.js
@@ -1,6 +1,14 @@
 import ReactDOM from 'react-dom';
+import { useEffect } from 'react';
 
 function Modal({ onClose, children, actionBar }) {
+	useEffect(() => {
+		document.body.classList.add('overflow-hidden');
+		return () => {
+			document.body.classList.remove('overflow-hidden');
+		};
+	}, []);
+
 	return ReactDOM.createPortal(
 		<div>
 			<div onClick={onClose}
