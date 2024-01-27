2c4121341727aab0cdb1c3e3fed9e384117a3b1d
kynoh <nohkr160428@gmail.com>
2024-01-27 17:22:53 +0900

    Apply HTML table structure to React

--- comps/src/components/Table.js
+++ comps/src/components/Table.js
@@ -1,8 +1,36 @@
 function Table({ data }) {
 	return(
-		<div>
-			{data.length}
-		</div>
+		<table>
+			<thead>
+				<tr>
+					<th>Fruits</th>
+					<th>Color</th>
+					<th>Score</th>
+				</tr>
+			</thead>
+			<tbody>
+				<tr>
+					<td>Orange</td>
+					<td>orange</td>
+					<td>5</td>
+				</tr>
+				<tr>
+					<td>Apple</td>
+					<td>red</td>
+					<td>3</td>
+				</tr>
+				<tr>
+					<td>Banana</td>
+					<td>yellow</td>
+					<td>1</td>
+				</tr>
+				<tr>
+					<td>Lime</td>
+					<td>green</td>
+					<td>4</td>
+				</tr>
+			</tbody>
+		</table>
 	);
 }
 
