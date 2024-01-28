f2fad254188c686a3a697853fa2117acaa236dda
kynoh <nohkr160428@gmail.com>
2024-01-28 23:50:48 +0900

    Make Controlled input element

--- comps/src/pages/CounterPage.js
+++ comps/src/pages/CounterPage.js
@@ -4,6 +4,7 @@ import Panel from '../components/Panel';
 
 function CounterPage({ initialCount }) {
 	const [count, setCount] = useState(initialCount);
+	const [valueToAdd, setValueToAdd] = useState(0);
 
 	const increment = () => {
 		setCount(count + 1);
@@ -11,6 +12,12 @@ function CounterPage({ initialCount }) {
 
 	const decrement = () => {
 		setCount(count - 1);
+	};
+
+	const handleChange = (event) => {
+		const value = event.target.value;
+
+		console.log(typeof value);
 	};
 
 	return (
@@ -22,7 +29,12 @@ function CounterPage({ initialCount }) {
 			</div>
 			<form>
 				<label>Add a lot!</label>
-				<input type="number" className="p-1 m-3 bg-gray-50 border border-gray-300" />
+				<input
+					value={valueToAdd}
+					onChange={handleChange}
+					type="number"
+					className="p-1 m-3 bg-gray-50 border border-gray-300"
+				/>
 				<Button>Add it!</Button>
 			</form>
 		</Panel>
