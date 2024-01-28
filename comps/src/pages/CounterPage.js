1d90c7e04a260bca643f8d13ae5eddc516d8ccb6
kynoh <nohkr160428@gmail.com>
2024-01-28 23:36:17 +0900

    Refactoring <CounterPage> to use <useReducer>
    Remove codes using cutom hook <useCounter>.
    Initialize <count> state with <useState>.
    Define event handler <increment>.
    Define event handler <decrement>.
    Add 'Decrement' Button.

--- comps/src/pages/CounterPage.js
+++ comps/src/pages/CounterPage.js
@@ -1,15 +1,22 @@
+import { useState } from 'react';
 import Button from '../components/Button'
-import useCounter from '../hooks/use-counter';
 
 function CounterPage({ initialCount }) {
-	const { count, increment } = useCounter(initialCount);
+	const [count, setCount] = useState(initialCount);
+
+	const increment = () => {
+		setCount(count + 1);
+	};
+
+	const decrement = () => {
+		setCount(count - 1);
+	};
+
 	return (
 		<div>
 			<h1>Count is {count}</h1>
-			<Button onClick={increment}>
-				Increment
-			</Button>
-
+			<Button onClick={increment}>Increment</Button>
+			<Button onClick={decrement}>Decrement</Button>
 		</div>
 	);
 }
