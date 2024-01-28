7160e343e3490e8a2dda50f8eb9f732bfb3bf5c7
kynoh <nohkr160428@gmail.com>
2024-01-28 23:44:02 +0900

    Implement form submission elements

--- comps/src/pages/CounterPage.js
+++ comps/src/pages/CounterPage.js
@@ -1,5 +1,6 @@
 import { useState } from 'react';
 import Button from '../components/Button'
+import Panel from '../components/Panel';
 
 function CounterPage({ initialCount }) {
 	const [count, setCount] = useState(initialCount);
@@ -13,11 +14,18 @@ function CounterPage({ initialCount }) {
 	};
 
 	return (
-		<div>
-			<h1>Count is {count}</h1>
+		<Panel className="m-3">
+			<h1 className="text-lg">Count is {count}</h1>
+			<div className="flex flex-row">
 			<Button onClick={increment}>Increment</Button>
 			<Button onClick={decrement}>Decrement</Button>
-		</div>
+			</div>
+			<form>
+				<label>Add a lot!</label>
+				<input type="number" className="p-1 m-3 bg-gray-50 border border-gray-300" />
+				<Button>Add it!</Button>
+			</form>
+		</Panel>
 	);
 }
 
