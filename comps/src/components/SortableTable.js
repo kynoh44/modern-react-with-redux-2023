862a586137b51d647f342c3b308696aa8bd3f4ef
kynoh <nohkr160428@gmail.com>
2024-01-28 17:05:18 +0900

    Implement data sorting in <SortableTable>

--- comps/src/components/SortableTable.js
+++ comps/src/components/SortableTable.js
@@ -4,7 +4,7 @@ import Table from "./Table"
 function SortableTable(props) {
 	const [sortOrder, setSortOrder] = useState(null);
 	const [sortBy, setSortBy] = useState(null);
-	const { config } = props;
+	const { config, data } = props;
 
 	const handleClick = (label) => {
 		if (sortOrder === null) {
@@ -34,8 +34,29 @@ function SortableTable(props) {
 		};
 	});
 
+	let sortedData = data;	// Make a copy of the 'data' prop for re-rendering by state updated
+	if (sortOrder && sortBy) {	// Only sort data if sortOrder && sortBy are not NULL!
+		// Find the correct sortValue function and use it for sorting
+		const { sortValue } = config.find(column => column.label === sortBy);
+		sortedData = [...data].sort((a, b) => {
+			const valueA = sortValue(a);
+			const valueB = sortValue(b);
+
+			const reverseOrder = sortOrder === 'asc' ? 1 : -1;
+
+			if (typeof valueA === 'string') {
+				return valueA.localeCompare(valueB) * reverseOrder;
+			} else {
+				return (valueA - valueB) * reverseOrder;
+			}
+		});
+	}
+
 	return (
-		<div>{sortOrder} - {sortBy}<Table {...props} config={updatedConfig} /></div>
+		<div>
+			{sortOrder} - {sortBy}
+			<Table {...props} data={sortedData} config={updatedConfig} />
+		</div>
 	);
 }
 
