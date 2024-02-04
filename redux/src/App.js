445486c925be280cda2cee5c7ea6bee136c59835
kynoh <nohkr160428@gmail.com>
2024-02-04 22:54:25 +0900

    Reset RTK's state values by myself

--- redux/src/App.js
+++ redux/src/App.js
@@ -1,10 +1,30 @@
+import { useDispatch, useSelector } from "react-redux";
 import "./styles.css";
 import MoviePlaylist from "./components/MoviePlaylist";
 import SongPlaylist from "./components/SongPlaylist";
+import { removeSong, removeMovie } from "./store";
 
 export default function App() {
+	const dispatch = useDispatch();
+
+	const moviePlaylist = useSelector((state) => {
+		return state.movies;
+	});
+	const songPlaylist = useSelector((state) => {
+		return state.songs;
+	});
+
+	//console.log(moviePlaylist.length, songPlaylist.length);
+
 	const handleResetClick = () => {
-		//
+		for (let i = 0; i < moviePlaylist.length + songPlaylist.length; i++) {
+			if (moviePlaylist.length !== 0) {
+				dispatch(removeMovie(moviePlaylist[i]));
+			}
+			if (songPlaylist.length !== 0) {
+				dispatch(removeSong(songPlaylist[i]));
+			}
+		}
 	};
 
 	return (
