545416281738e56c219eaa25a3d2f9ad2a0660a2
kynoh <nohkr160428@gmail.com>
2024-02-09 22:57:50 +0900

    Use Redux store and action creator function in <CarForm> component

--- cars/src/components/CarForm.js
+++ cars/src/components/CarForm.js
@@ -12,7 +12,7 @@ function CarForm() {
 	};
 
 	return (
-		<div className="car-form panal">
+		<div className="car-form panel">
 			<h4 className="subtitle is-3">Add Car</h4>
 			<form>
 				<div className="field-group">
