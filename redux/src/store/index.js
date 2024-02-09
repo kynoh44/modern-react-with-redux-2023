88176f73cd8a4a36682ca9b16eca357ed07a1594
kynoh <nohkr160428@gmail.com>
2024-02-09 16:41:22 +0900

    Refactoring <songsSlice> to <SongsSlice.js>
    Paste <songsSlice> to <SongsSlice.js>.
    Import <createSlice> in <SongsSlice.js>.
    Import <reset> in <SongsSlice.js>
    
    Export <addSong> and <removeSong> actionCreators as named export.
    Export <songsReducer> as named export.

--- redux/src/store/index.js
+++ redux/src/store/index.js
@@ -1,25 +1,5 @@
 import { configureStore, createSlice, createAction } from "@reduxjs/toolkit";
 
-const songsSlice = createSlice({
-	name: 'song',
-	initialState: [],
-	reducers: {
-		addSong(state, action) {
-			state.push(action.payload);
-		},
-		removeSong(state, action) {
-			// action.payload === string (the song we want to remove)
-			const index = state.indexOf(action.payload);
-			state.splice(index, 1);
-		},
-	},
-	extraReducers(builder) {
-		builder.addCase(reset, (state, action) => {
-			return [];
-		});
-	},
-});
-
 const store = configureStore({
 	reducer: {
 		songs: songsSlice.reducer,
