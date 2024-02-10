a82c39c077a6a86be53a6ad8887f81897a7a2b3f
kynoh <nohkr160428@gmail.com>
2024-02-10 17:49:22 +0900

    Use <reduce> function for accumulation

--- cars/src/components/CarValue.js
+++ cars/src/components/CarValue.js
@@ -6,13 +6,9 @@ function CarValue() {
 			return car.name.toLowerCase().includes(searchTerm.toLowerCase());
 		});
 
-		let cost = 0;
-
-		for (let car of filteredCars) {
-			cost += car.cost;
-		}
-
-		return cost;
+		return filteredCars.reduce((acc, car) => {
+			return acc + car.cost;
+		}, 0);
 	});
 
 	return (
