7385f2492520d26c4a14ac67d71a830f534661a1
kynoh <nohkr160428@gmail.com>
2024-02-10 17:55:33 +0900

    Condense <totalCost>

--- cars/src/components/CarValue.js
+++ cars/src/components/CarValue.js
@@ -1,13 +1,13 @@
 import { useSelector } from "react-redux";
 
 function CarValue() {
-	const totalCost = useSelector(({ cars: { data, searchTerm } }) => {
-		const filteredCars = data.filter((car) => {
-			return car.name.toLowerCase().includes(searchTerm.toLowerCase());
-		});
-
-		return filteredCars.reduce((acc, car) => acc + car.cost, 0);
-	});
+	const totalCost = useSelector(({ cars: { data, searchTerm } }) =>
+		data
+			.filter((car) =>
+				car.name.toLowerCase().includes(searchTerm.toLowerCase())
+			)
+			.reduce((acc, car) => acc + car.cost, 0)
+	);
 
 	return (
 		<div className="car-value">
