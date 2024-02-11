ecc44613ba2ae382c3c54849cf3a9458a4ea5ea1
kynoh <nohkr160428@gmail.com>
2024-02-11 22:35:50 +0900

    Specify style argument to <classNames> function

--- media/src/components/Skeleton.js
+++ media/src/components/Skeleton.js
@@ -1,8 +1,23 @@
 import classNames from 'classnames';
 
 function Skeleton({ times }) {
-	const outerClassNames = classNames();
-	const innerClassNames = classNames();
+	const outerClassNames = classNames(
+		'relative',
+		'overflow-hidden',
+		'bg-gray-200',
+		'rounded',
+		'mb-2.5',
+	);
+	const innerClassNames = classNames(
+		'animate-shimmer',
+		'absolute',
+		'inset-0',
+		'-translate-x-full',
+		'bg-gradient-to-r',
+		'from-gray-200',
+		'via-white',
+		'to-gray-200',
+	);
 
 	const boxes = Array(times).fill(0).map((_, i) => {
 		return (
