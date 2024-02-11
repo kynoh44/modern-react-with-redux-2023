c1f1e8b4a0b516859d5cbf2c3ec1815168886fb1
kynoh <nohkr160428@gmail.com>
2024-02-11 22:29:20 +0900

    Add divisions in <Skeleton> component

--- media/src/components/Skeleton.js
+++ media/src/components/Skeleton.js
@@ -1,8 +1,16 @@
 import classNames from 'classnames';
 
 function Skeleton({ times }) {
+	const outerClassNames = classNames();
+	const innerClassNames = classNames();
+
 	const boxes = Array(times).fill(0).map((_, i) => {
-		return <div key={i} />;
+		return (
+			<div key={i} className={outerClassNames}>
+				<div className={innerClassNames}>
+
+				</div>
+			</div>);
 	});
 
 	return boxes;
