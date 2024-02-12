14e81087dfdc982ef59bad4e0b61bdf951c04968
kynoh <nohkr160428@gmail.com>
2024-02-13 00:03:41 +0900

    Connect API to Redux Store : Combined reducer

--- media/src/store/index.js
+++ media/src/store/index.js
@@ -1,9 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
+import { setupListeners } from "@reduxjs/toolkit/query";
 import { usersReducers } from "./usersSlice";
+import { albumsApi } from "./apis/albumsApi";
 
 export const store = configureStore({
 	reducer: {
-		users: usersReducers
+		users: usersReducers,
+		albums: albumsApi.reducer,
 	},
 });
 
