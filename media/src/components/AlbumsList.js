657c3a3c3f731658828688fa6cf0fea7ae3a0304
kynoh <nohkr160428@gmail.com>
2024-02-13 00:38:34 +0900

    Use RTK Query hook in <AlbumList> component

--- media/src/components/AlbumsList.js
+++ media/src/components/AlbumsList.js
@@ -1,4 +1,8 @@
+import { useFetchAlbumsQuery } from "../store";
+
 function AlbumsList({ user }) {
+	const { data, error, isLoading } = useFetchAlbumsQuery(user);
+
 	return (
 		<div>
 			Albums for {user.name}
