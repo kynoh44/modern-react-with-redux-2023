96d4654bbfc13e02b8d92385bf03495a4e1f6aab
kynoh <nohkr160428@gmail.com>
2024-02-12 12:42:00 +0900

    Implement fine-grained loading state by option 1

--- media/src/components/UsersList.js
+++ media/src/components/UsersList.js
@@ -1,30 +1,36 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { fetchUsers, addUser } from "../store";
 import Button from "./Button";
 import Skeleton from "./Skeleton";
 
 function UsersList() {
+	const [isLoadingUsers, setIsLoadingUsers] = useState(false);
+	const [loadingUsersError, setLoadingUsersError] = useState(null);
+
 	const dispatch = useDispatch();
-	const { isLoading, data, error } = useSelector((state) => {
+	const { data } = useSelector((state) => {
 		return state.users;	// { data: [], isLoading: false, error: null }
 	});
 
 	useEffect(() => {
+		setIsLoadingUsers(true);
 		console.log(dispatch(fetchUsers()));
+		// BAD!!
+		//setIsLoadingUsers(false);
 	}, [dispatch]);
 
 	const handleUserAdd = () => {
 		dispatch(addUser());
 	};
 
-	if (isLoading) {
+	if (isLoadingUsers) {
 		return (
 			<Skeleton times={6} className="h-10 w-full" />
 		);
 	}
 
-	if (error) {
+	if (loadingUsersError) {
 		return (
 			<div>
 				Error fetching data ...
