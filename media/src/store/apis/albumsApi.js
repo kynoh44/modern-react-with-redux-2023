f6a86eb801b9c35a572503e530203425bb1e27df
kynoh <nohkr160428@gmail.com>
2024-02-12 22:56:00 +0900

    Add <baseQuery> property and ... Import <fetchBaseQuery>. Specify <baseUrl> property with API server hosting directory as the configuration object of <fetchBaseQuery> function.

--- media/src/store/apis/albumsApi.js
+++ media/src/store/apis/albumsApi.js
@@ -1,5 +1,8 @@
-import { createApi } from '@reduxjs/toolkit/query/react';
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 
 const albumsApi = createApi({
 	reducerPath: 'albums',
+	baseQuery: fetchBaseQuery({
+		baseUrl: 'http://localhost:3005'
+	}),
 });
