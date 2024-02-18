00b39e3944098d8f410c5087c6fc07b5f518b6f6
kynoh <nohkr160428@gmail.com>
2024-02-18 12:08:57 +0900

    Implement <removeAlbum> query

--- media/src/store/apis/albumsApi.js
+++ media/src/store/apis/albumsApi.js
@@ -54,9 +54,21 @@ const albumsApi = createApi({
 					};
 				},
 			}),
+			removeAlbum: builder.mutation({
+				query: (album) => {
+					return {
+						url: `/albums/${album.id}`,
+						method: 'DELETE',
+					};
+				},
+			}),
 		};
 	},
 });
 
-export const { useFetchAlbumsQuery, useAddAlbumMutation } = albumsApi;
+export const {
+	useFetchAlbumsQuery,
+	useAddAlbumMutation,
+	useRemoveAlbumMutation
+} = albumsApi;
 export { albumsApi };
