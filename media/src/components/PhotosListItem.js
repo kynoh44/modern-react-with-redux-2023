a81f8aa05c4860ae244e7aa31d2376b9adbf9d59
kynoh <nohkr160428@gmail.com>
2024-02-18 16:46:32 +0900

    Display photo image in <PhotosListItem> component

--- media/src/components/PhotosListItem.js
+++ media/src/components/PhotosListItem.js
@@ -1,7 +1,7 @@
-function PhotosListItem() {
+function PhotosListItem({ photo }) {
 	return (
 		<div>
-			PhotosListItem
+			<img className="h-20 w-20" src={photo.url} alt="random pic" />
 		</div>
 	);
 }
